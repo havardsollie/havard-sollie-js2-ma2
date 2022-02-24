d77d8c3ceafa2f1d74c40cd6dd5d59ba4a09071d
havardsollie <79274753+havardsollie@users.noreply.github.com>
2022-02-24 10:32:35 +0100

    level 1

--- js/script.js
+++ js/script.js
@@ -22,54 +22,47 @@ let books = [
 ];
 
 
-const container = document.querySelector(".book-container");
+const container = document.querySelector("ul");
 
-for (let i = 0; i < books.length; i++) {
-    
-    let isbn = books[i].isbn;
-    let title = books[i].title;
-    let colour = books[i].colour;
+function createList() {
+    for (let i = 0; i < books.length; i++) {
+      let isbn = books[i].isbn;
+      let title = books[i].title;
+      let colour = books[i].colour;
+      container.innerHTML += `<li>
+          <p>ISBN: ${isbn}</p><h4 style="color:${colour}">${title}</h4><button class="btn" data-item="${isbn}">
+          </li>` 
+    }
 
+    const trashCan = document.querySelectorAll("button");
 
-
-    function createList() {
-    
-        container.innerHTML += `
-        <p>${isbn}</p><h4 style="color:${colour}">${title}</h4><button class="btn" data-item="${isbn}">
-        ` 
-        
-        const trashCan = document.querySelectorAll("button");
-
-        trashCan.forEach(function(can) {
-            can.addEventListener("click", removeFromList);
-        })
-}
-
+    trashCan.forEach(function(can) {
+        can.addEventListener("click", removeFromList);
+    })
+  }
 
 createList();
 
 
 
-
 function removeFromList(event) {
     const itemToRemove = event.target.dataset.item;
+    container.innerHTML = "";
 
     const newList = books.filter(function(book) {
         return book.isbn !== itemToRemove;
     })
     
     console.log(event.target.dataset.item);
-    console.log(isbn);
     console.log(newList);
     console.log(itemToRemove);
     
     books = newList;
 
-    createList();
+    createList(newList);
 
 }
 
-}
 
 
 
